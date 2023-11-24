d27f0ba42c61abf74fb518d40f09669d72067283
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-24 04:24:46 +0000

    Create a procedure that calculates average

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,14 @@
+-- Compute and store the average score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN input_user_id INT)
+BEGIN
+  DECLARE result INT DEFAULT 0;
+  SELECT AVG(score) INTO result
+  FROM corrections
+  WHERE user_id = input_user_id;
+  UPDATE users SET average_score = result WHERE id = input_user_id;
+END
+$$
