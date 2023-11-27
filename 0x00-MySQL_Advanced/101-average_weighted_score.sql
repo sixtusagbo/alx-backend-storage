a5c6729ef42f8b03b3a7f2e21b109e69908bd23f
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-27 22:12:53 +0000

    List all databases

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,6 +1,23 @@
 -- Calculate average weighted score for all users
 DELIMITER $$
 
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN in_user_id INT)
+BEGIN
+  DECLARE sum_weight INT;
+  DECLARE sum_score_weight DOUBLE;
+  SELECT SUM(weight) INTO sum_weight
+  FROM corrections
+    JOIN projects
+      WHERE id = project_id AND user_id = in_user_id;
+  SELECT SUM(score * weight) INTO sum_score_weight
+  FROM corrections
+    JOIN projects
+      WHERE id = project_id AND user_id = in_user_id;
+  UPDATE users
+    SET average_score = sum_score_weight / sum_weight
+    WHERE id = in_user_id;
+END$$
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
   DECLARE done INT DEFAULT FALSE;
