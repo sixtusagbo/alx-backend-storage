56bf6d02f6d9abf36a7119533bb1b56756d08ae9
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-11-29 08:48:27 -0800

    List all documents in Python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Lists all documents in a collection"""
+
+def list_all(mongo_collection):
+    """Return a list of all documents in a collection"""
+    return list(mongo_collection.find())
